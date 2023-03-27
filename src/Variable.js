52525eb9d77417e941a2a7d4f4eb83beacdc5cc5
thiziri kechidi <thiziri.kechidi@etu.univ-lehavre.fr>
2023-03-27 18:27:27 +0200

    add varaible file  of importation and creation of elements

---
+++ src/Variable.js
@@ -0,0 +1,4 @@
+export const messages = document.querySelector("#messages");
+export const aside = document.querySelector("#aside");
+export const room = document.createElement("input");
+export const zoneDessin = document.createElement("canvas");
